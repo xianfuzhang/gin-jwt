f1321879011299f11cff7824d72ea835e8b4c6dc
xianfu.zhang <286330850@qq.com>
2023-03-09 11:14:05 +0800

    gin framework with jwt auth http request

---
+++ auth/auth.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("nocsys")
+
+type JWTClaim struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(username string) (tokenString string, err error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &JWTClaim{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err = token.SignedString(jwtKey)
+	return
+}
+func ValidateToken(signedToken string) (err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JWTClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(jwtKey), nil
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	_, ok := token.Claims.(*JWTClaim)
+	if !ok {
+		err = errors.New("couldn't parse claims")
+		return
+	}
+	return nil
+}
---
+++ controllers/user.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"net/http"
+	"test/v2/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUser(context *gin.Context) {
+	var user models.User
+	if err := context.ShouldBindJSON(&user); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	if err := user.HashPassword(user.Password); err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	// record := database.Instance.Create(&user)
+	// if record.Error != nil {
+	// 	context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
+	// 	context.Abort()
+	// 	return
+	// }
+	context.JSON(http.StatusCreated, gin.H{"username": user.Name})
+}
